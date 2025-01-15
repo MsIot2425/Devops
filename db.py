8a530192e4dfd69f3460b1a696822254712ea653
MsIot2425 <ms.iot2425@gmail.com>
2025-01-15 10:24:09 -0400

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from config import Config
-
-def get_db_connection():
-    try:
-        return psycopg2.connect(**Config.DB_SETTINGS)
-    except psycopg2.Error as e:
-        raise RuntimeError(f"Erreur de connexion à la base de données : {e}")
